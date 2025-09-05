5ebbe25a7f7ac2dcb409dd6d0e0591b12303afbd
TEACHER <TEACHER@rivne.itstep.org>
2025-09-05 11:45:08 +0300

    Read file in Student list

--- Lesson 2. Simple Class/ExampleClass/ExampleClass/Program.cs
+++ Lesson 2. Simple Class/ExampleClass/ExampleClass/Program.cs
@@ -14,19 +14,60 @@ Console.OutputEncoding = Encoding.UTF8;
 //Console.WriteLine("Привіт Запоріжжя!");
 
 //Створюємо змінну типу Student, яка називається yosup.
-//слово new - виділяємо память під об'єкт yosup
-Student yosup = new Student();
-Console.WriteLine("Як Вас звати?");
-yosup.name = Console.ReadLine(); //Змінні в середині класу називаються поля
-Console.WriteLine("Який у Вас вік?");
-yosup.age = int.Parse(Console.ReadLine()); //присвоюємо значення 18
-Console.WriteLine("Ви зараховані на навчання :)");
-Console.WriteLine($"{yosup.name} {yosup.age}");
 
-//Маю файл і режим роботи буде дозапис
-//Вміст файлу видаляти не буде. Завжди дописуємо
-StreamWriter sw =
-    new StreamWriter("students.txt", true);
-sw.WriteLine(yosup.name);
-sw.WriteLine(yosup.age);
-sw.Close();
+int action = 0;
+do
+{
+    Console.WriteLine("Оберіть операцію:");
+    Console.WriteLine("0.Вихід");
+    Console.WriteLine("1.Додати студента у файл");
+    Console.WriteLine("2.Показати список студентів на навчанні");
+    Console.Write("->_");
+    action = int.Parse(Console.ReadLine());
+    switch(action)
+    {
+        case 1:
+            {
+                //слово new - виділяємо память під об'єкт yosup
+                Student yosup = new Student();
+                Console.WriteLine("Як Вас звати?");
+                yosup.name = Console.ReadLine(); //Змінні в середині класу називаються поля
+                Console.WriteLine("Який у Вас вік?");
+                yosup.age = int.Parse(Console.ReadLine()); //присвоюємо значення 18
+                Console.WriteLine("Ви зараховані на навчання :)");
+                Console.WriteLine($"{yosup.name} {yosup.age}");
+
+                //Маю файл і режим роботи буде дозапис
+                //Вміст файлу видаляти не буде. Завжди дописуємо
+                StreamWriter sw =
+                    new StreamWriter("students.txt", true);
+                sw.WriteLine(yosup.name);
+                sw.WriteLine(yosup.age);
+                sw.Close();
+                break;
+            }
+        case 2:
+            {
+                StreamReader sr = new StreamReader("students.txt");
+                string str = sr.ReadLine();
+                while(!string.IsNullOrEmpty(str))
+                {
+                    //Console.WriteLine(str);
+                    if(!string.IsNullOrEmpty(str))
+                    {
+                        //Якщо вдалося прочитати name
+                        Student temp = new Student();
+                        temp.name = str;
+                        temp.age = int.Parse(sr.ReadLine()); //Читаю вік студента
+                        Console.WriteLine($"{temp.name} -- {temp.age}");
+                    }
+                    str = sr.ReadLine();
+                }
+                sr.Close(); //Ми зкриваємо потік читання файлу. 
+                break;
+            }
+    }
+
+} while (action != 0);
+
+
