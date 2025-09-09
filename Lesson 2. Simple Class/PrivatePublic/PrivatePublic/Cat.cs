3a10834209aa3732c70bbffa09278b974a363d01
TEACHER <TEACHER@rivne.itstep.org>
2025-09-09 13:23:32 +0300

    Working clases

---
+++ Lesson 2. Simple Class/PrivatePublic/PrivatePublic/Cat.cs
@@ -0,0 +1,45 @@
+﻿namespace PrivatePublic;
+
+class Cat
+{
+    /// <summary>
+    /// Назва кота
+    /// </summary>
+    private string name;
+    /// <summary>
+    /// Вік
+    /// </summary>
+    private int age;  //не хочу давати public
+    /// <summary>
+    /// Порода кота
+    /// </summary>
+    private string breed;
+    //this - звертаюся до даного класу
+    public void setName(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Ім'я котима не може бути постим (:(:(:(:");
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }
+        this.name = name;
+    }
+    public string getName() { return name; }
+
+    public void setAge(int age) 
+    {
+        if (age < 0)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Даний коти - ще не родився (:");
+            Console.ForegroundColor = ConsoleColor.White;
+            return;
+        }    
+        this.age = age; 
+    }
+    public int getAge() { return age; }
+
+
+}
---
+++ Lesson 2. Simple Class/PrivatePublic/PrivatePublic/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PrivatePublic;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Cat мурчик = new Cat(); //Створив змінну для Сat - мурчик - об'єкт, екземпляр
+
+Console.WriteLine("Вкажіть ім'я кота");
+string name = Console.ReadLine();
+мурчик.setName(name);
+Console.WriteLine(мурчик.getName());
+
+Console.WriteLine("Вкажіть вік котика");
+int age = int.Parse(Console.ReadLine());
+мурчик.setAge(age);
+Console.WriteLine(мурчик.getAge());
+//Console.WriteLine("Привіт козак!");
