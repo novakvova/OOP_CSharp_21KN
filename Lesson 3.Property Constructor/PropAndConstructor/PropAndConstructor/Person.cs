cf7d27c27de50327c019b252dbe00e05c8391eac
TEACHER <TEACHER@rivne.itstep.org>
2025-09-16 13:17:14 +0300

    Working constructor

---
+++ Lesson 3.Property Constructor/PropAndConstructor/PropAndConstructor/Person.cs
@@ -0,0 +1,50 @@
+﻿namespace PropAndConstructor;
+
+public class Person
+{
+    private string _id = null!;
+    private string _firstName = null!;
+    private string _lastName = null!;
+
+    public string LastName
+    {
+        get { return _lastName; }
+        set { _lastName = value; }
+    }
+
+    public string Phone { get; set; }
+
+    //Визивається при створені класа.
+    //Змінні набувають початкового значення
+    //Конструктор по замовлюванню 
+    public Person()
+    {
+        //GUID - рандомна величина, яка не може повторитися
+        _id = Guid.NewGuid().ToString();
+    }
+
+    //Конструктор з параметрами
+    public Person(string lastName, string firstname, string phone) : this() 
+    {
+        _lastName = lastName;
+        _firstName = firstname;
+        Phone = phone;
+    }
+
+    //Повертає значення _id
+    public string Id { get { return _id; } }
+
+    public void setFirstName(string firstName)
+    { 
+        _firstName = firstName; 
+    }
+    public string getFirstName()
+    {
+        return _firstName;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} -- {LastName} {_firstName} -> {Phone}";
+    }
+}
---
+++ Lesson 3.Property Constructor/PropAndConstructor/PropAndConstructor/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PropAndConstructor;
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+Console.WriteLine("Це конструктори!");
+
+Person semen = new Person();
+
+semen.setFirstName("Вітя");
+
+semen.LastName = "Марков"; //визиваємо метод set
+
+semen.Phone = "+38(098) 22 33 445";
+
+Person ivanka = new Person("Борода","Люба", "+109 803 44 55");
+
+//Console.WriteLine(semen.getFirstName());
+//Console.WriteLine(semen.LastName); //визиваємо метод get
+//Console.WriteLine(semen.Phone); //визиваємо метод get
+
+Console.WriteLine(semen);
+Console.WriteLine(ivanka);
