b567732a09249793941c7d4fb5ced799909d4d3c
TEACHER <TEACHER@rivne.itstep.org>
2025-09-19 10:51:10 +0300

    Working app

---
+++ Lesson 4.Static Method/ExampleStatic/ExampleStatic/Man.cs
@@ -0,0 +1,63 @@
+﻿namespace ExampleStatic;
+
+public class Man
+{
+    private string _firstName;
+
+    public string FirstName
+    {
+        get { return _firstName; }
+        set { _firstName = value; }
+    }
+
+    private string _lastName;
+
+    public string LastName
+    {
+        get { return _lastName; }
+        set { _lastName = value; }
+    }
+
+    public int Age { get; set; }
+
+    //Статичне поле для зберіження кількості 
+    //створених об'єктів(екземплярів)
+    private static int _counter = 0;
+
+    //ctor - створити конструкто по замовчуванню
+    public Man()
+    {
+        Man._counter++;
+    }
+
+    /// <summary>
+    /// Ми зробили конструктор з параметрами, 
+    /// який викликає конструктор по замовчуванню
+    /// </summary>
+    /// <param name="firstName">Ім'я</param>
+    /// <param name="lastName">Прізвище</param>
+    /// <param name="age">Вік</param>
+    public Man(string firstName, string lastName, int age)
+        : this() //виклик конструктора по замовчуванню
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
+
+    public static void Info()
+    {
+        Console.WriteLine("На клас для роботи Man :)");
+    }
+
+    public static int GetCounter()
+    {
+        return _counter;
+    }
+    
+    public override string ToString()
+    {
+        return $"Прізвище: {LastName}, Ім'я: {FirstName}, Вік: {Age}";
+    }
+
+}
---
+++ Lesson 4.Static Method/ExampleStatic/ExampleStatic/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Bogus;
+using ExampleStatic;
+using System.Text;
+
+//static - дозволяє викликати методи класу без потреба створенне
+// екземпляру класу або об'єкту
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+
+//Ctrl + / - там біля shift правий
+//Console.WriteLine("Скільки Вам рочків?");
+//int age = int.Parse(Console.ReadLine());
+
+//Console.WriteLine($"age = {age}");
+
+//Ми використали клас Faker з бібліотеки Bogus
+Faker faker = new Faker("uk"); 
+string lastName = faker.Name.LastName(); //Прізвище 
+string firstName = faker.Name.FirstName(); //Ім'я
+int age = faker.Random.Int(18, 60); //Вік від 18 до 60 років
+
+Man ivan = new Man();
+Man petro = new Man();
+Man vova = new Man(firstName,lastName, age);
+
+Console.WriteLine(vova);
+
+Man.Info();
+int count = Man.GetCounter();
+Console.WriteLine("Counter Man = {0}", count);
