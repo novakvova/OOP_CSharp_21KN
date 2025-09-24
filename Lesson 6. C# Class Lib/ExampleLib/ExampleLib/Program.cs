04b9712a1e83654e1fe0e143f47830a1e7e417c9
TEACHER <TEACHER@rivne.itstep.org>
2025-09-24 09:27:15 +0300

    Add Lib

---
+++ Lesson 6. C# Class Lib/ExampleLib/ExampleLib/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyCalculator;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Привіт козаки і козачки");
+Calculator calc = new Calculator();
+calc.PrintInfo();
---
+++ Lesson 6. C# Class Lib/ExampleLib/MyCalculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace MyCalculator;
+
+public class Calculator
+{
+    public void PrintInfo()
+    {
+        Console.WriteLine("Це є найпростіший калькулятор :)");
+    }
+
+}
