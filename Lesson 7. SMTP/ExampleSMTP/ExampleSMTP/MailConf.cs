7bbdffd4ca64a8866d3b8662451af0d4fa889d44
TEACHER <TEACHER@rivne.itstep.org>
2025-09-26 11:50:11 +0300

    Working is good app

---
+++ Lesson 7. SMTP/ExampleSMTP/ExampleSMTP/MailConf.cs
@@ -0,0 +1,32 @@
+﻿namespace ExampleSMTP;
+
+/// <summary>
+/// Конфігурація пошти
+/// </summary>
+public static class MailConf
+{
+    /// <summary>
+    /// Хто надсилає листа
+    /// </summary>
+    public static string From { get; set; } = "super.novakvova@ukr.net";
+
+    /// <summary>
+    /// Назва серверу на ukr.net 
+    /// </summary>
+    public static string SmtpServer { get; set; } = "smtp.ukr.net";
+
+    /// <summary>
+    /// Порт, через який ми будемо працювати на ukr.net
+    /// </summary>
+    public static int SmtpPort { get; set; } = 2525;
+
+    /// <summary>
+    /// Ім'я користувача для авторизації
+    /// </summary>
+    public static string UserName { get; set; } = "super.novakvova@ukr.net";
+
+    /// <summary>
+    /// Пароль для доступу до ukr.net
+    /// </summary>
+    public static string Password { get; set; } = "j22yJ7sD5PAb4ewk";
+}
