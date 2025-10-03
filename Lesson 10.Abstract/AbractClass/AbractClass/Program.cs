77358ff004048a4a5da9fe98cacb53878b25eb53
novakvova <novakvova@gmail.com>
2025-10-03 09:40:30 +0300

    Add Project AbstractClass

---
+++ Lesson 10.Abstract/AbractClass/AbractClass/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+
+Console.InputEncoding = Encoding.UTF8;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Добрий ранок!");
