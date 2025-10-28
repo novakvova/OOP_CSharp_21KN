f98c26865dd204a90b9b075e4341693fed348714
novakvova <novakvova@gmail.com>
2025-10-28 13:10:55 +0200

    Working app delegate

---
+++ Lesson 21.Delegate/WorkingDelegate/WorkingDelegate/MyCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkingDelegate;
+
+public delegate int MySuperDelegate(int x, int y);
+
+public class MyCalculator
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+    public static int Minus(int a, int b)
+    {
+        return a - b;
+    }
+}
---
+++ Lesson 21.Delegate/WorkingDelegate/WorkingDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WorkingDelegate;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Привіт козаки і козачки!");
+
+
+
+//int result = MyCalculator.Add(5, 10); //виклик методу
+//Console.WriteLine("Result = " + result);
+Console.WriteLine("Оберіть операцію ");
+Console.WriteLine("1.Додавання");
+Console.WriteLine("2.Віднімання");
+Console.Write("->_");
+int action = int.Parse(Console.ReadLine()!);
+MySuperDelegate? del = null;
+if(action == 1)
+    del = new MySuperDelegate(MyCalculator.Add); //створення делегата
+else if(action == 2)
+    del = new MySuperDelegate(MyCalculator.Minus);
+
+if(del != null)
+{
+    //int result = del(20, 10); //виклик методу через делегат
+    int result = del.Invoke(20, 10); //виклик методу через делегат
+    Console.WriteLine("Результат роботи " + result);
+}
+
+//делегат - це вказівник на метод
+//Тобто делегат може вказувати на певний метод, а потім
+//цей метод можна викликати через делегат
