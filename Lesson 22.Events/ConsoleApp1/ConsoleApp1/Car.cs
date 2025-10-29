879b6b168b0f21e856d3470df0755858a61d8bcb
novakvova <novakvova@gmail.com>
2025-10-29 10:43:44 +0200

    Is Good working delegate

---
+++ Lesson 22.Events/ConsoleApp1/ConsoleApp1/Car.cs
@@ -0,0 +1,78 @@
+﻿using System.Security.Cryptography;
+
+namespace ConsoleApp1;
+
+public class Car
+{
+    //Даний делегат повідомляє інформацію про автомобіль
+    public delegate void DisplayInfoDelegate(string info);
+    public delegate void DisplayProplemInfoDelegate(string info);
+    //Поле для зберігання посилання на метод делегата
+    //Повідомляє на дисплей автомобіля інформацію
+    private DisplayInfoDelegate? displayInfoCallback;
+    //Повідомляє на дисплей автомобіля інформацію про проблеми
+    //private DisplayProplemInfoDelegate? displayProplemInfoCallback;
+	public event DisplayProplemInfoDelegate? displayProplemInfoCallback;
+
+    private string? model; //може зберігати null
+	private int speed;
+	private int maxSpeed;
+
+    public Car()
+    {
+        maxSpeed = 200;
+    }
+    public Car(string? model, int maxSpeed)
+	{
+		this.model = model;
+		this.maxSpeed = maxSpeed;
+		speed = 0;
+    }
+
+	public void RegisterDisplayAuto(DisplayInfoDelegate? displayInfoCallback)
+	{
+        //Реєструємо метод делегата, тобто у авто є дисплей
+        this.displayInfoCallback = displayInfoCallback;
+    }
+
+	//public void RegisterDisplayProplemAuto(DisplayProplemInfoDelegate? displayProplemInfoCallback)
+	//{
+	//	//Реєструємо метод делегата, тобто у авто є дисплей проблем
+	//	this.displayProplemInfoCallback = displayProplemInfoCallback;
+ //   }
+
+    public void Run(int speed)
+	{
+		this.speed += speed;
+		if(this.speed > 0)
+		{ 
+			if(displayInfoCallback != null)
+			{
+				displayInfoCallback($"Автомобіль {model} рухається зі швидкістю {this.speed} км/год");
+            }
+        }
+		if(this.speed >= maxSpeed)
+		{
+            displayProplemInfoCallback?.Invoke($"Автомобіль {model} досяг максимальної швидкості {maxSpeed} км/год");
+
+   //         if (displayProplemInfoCallback != null)
+			//{
+			//	displayProplemInfoCallback($"Автомобіль {model} перевищив максимальну швидкість {maxSpeed} км/год");
+   //         }
+			speed = maxSpeed;
+        }
+    }
+
+
+    public int Speed
+	{
+		get { return speed; }
+	}
+
+	public string? Model
+	{
+		get { return model; }
+		set { model = value; }
+	}
+
+}
---
+++ Lesson 22.Events/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+//Console.WriteLine("Ще не вмерла Україна");
+void DisplayAutoInfo(string info) //Диплей автомобіля
+{
+    Console.WriteLine(info);
+}
+void DisplayAutoErrorInfo(string info) //Диплей автомобіля
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("-----------------");
+    Console.WriteLine(info);
+    Console.WriteLine("-----------------");
+    Console.ForegroundColor = ConsoleColor.White;
+}
+//Створюємо об'єкт автомобіля
+Car car = new Car("BMW", 240);
+car.RegisterDisplayAuto(DisplayAutoInfo); //підключили дисплей для авто
+car.RegisterDisplayProplemAuto(DisplayAutoErrorInfo); //підключили дисплей для проблем авто
+car.Run(50);
+car.Run(200); //збільшили швидкість на 200 км/год
