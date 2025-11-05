7557226c2135838f1825b9ed2d3bc26a432008d4
novakvova <novakvova@gmail.com>
2025-11-05 17:34:40 +0200

    Testing stopwatch

--- Lesson 25 LoadingImage DataGridView/LoadingImageUrl/LoadingImageUrl/MainForm.cs
+++ Lesson 25 LoadingImage DataGridView/LoadingImageUrl/LoadingImageUrl/MainForm.cs
@@ -13,9 +13,31 @@ namespace LoadingImageUrl
 
         private void btnLoadUrl_Click(object sender, EventArgs e)
         {
+            LoadImage();
+        }
+        public async Task LoadImage()
+        {
             Stopwatch stopWatch = new Stopwatch();
             stopWatch.Start();
-            LoadImage();
+            int n = 5;
+            for(int i=0; i < n; i++)
+            {
+                byte[] imageBytes = await client.GetByteArrayAsync(imgUrl);
+                using var ms = new MemoryStream(imageBytes);
+
+                pbImage.Image = Image.FromStream(ms);
+
+                var dir = Path.Combine(Directory.GetCurrentDirectory(), "images");
+                if (!Directory.Exists(dir))
+                {
+                    Directory.CreateDirectory(dir);
+                }
+                string fileSave = Path.Combine(dir, "image"+i+".jpg");
+                FileStream fs = new FileStream(fileSave, FileMode.Create);
+                fs.Write(ms.ToArray());
+                fs.Close();
+            }
+            //photo = Image.FromStream(ms);
             stopWatch.Stop();
             // Get the elapsed time as a TimeSpan value.
             TimeSpan ts = stopWatch.Elapsed;
@@ -24,14 +46,6 @@ namespace LoadingImageUrl
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds / 10);
             MessageBox.Show("RunTime " + elapsedTime);
-        }
-        public async Task LoadImage()
-        {
-            byte[] imageBytes = await client.GetByteArrayAsync(imgUrl);
-            using var ms = new MemoryStream(imageBytes);
-
-            pbImage.Image = Image.FromStream(ms);
-                //photo = Image.FromStream(ms);
         }
     }
 }
