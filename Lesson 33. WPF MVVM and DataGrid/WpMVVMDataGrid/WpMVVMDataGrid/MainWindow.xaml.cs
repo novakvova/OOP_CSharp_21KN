0f3a5c108185821ac49ac8e0b6df6e289ea70131
novakvova <novakvova@gmail.com>
2025-11-21 10:56:08 +0200

    Is good working app

---
+++ Lesson 33. WPF MVVM and DataGrid/WpMVVMDataGrid/WpMVVMDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+using WpMVVMDataGrid.Models;
+
+namespace WpMVVMDataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Спеціальна колекція, яка підтримує
+        // сповіщення View про зміни у колекції(додавання, видалення елементів)
+        private ObservableCollection<UserVM> users = new ();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Заповнюємо колекцію users
+            dgUsers.ItemsSource = users;
+        }
+
+        private void btnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+            UserVM newUser = new UserVM();
+            newUser.Id = users.Count + 1;
+            newUser.Name = "New User " + newUser.Id;
+            newUser.Image = "https://i.pinimg.com/originals/43/06/3c/43063c8162fbbae1cf37df742d2afa1c.jpg";
+
+            users.Add(newUser);
+            //MessageBox.Show("Додати нового користувача");
+        }
+
+        private void btnEditUser_Click(object sender, RoutedEventArgs e)
+        {
+            users[0]
+                .Image = "https://assets.dots.live/misteram-public/01978356-8ff2-7017-bbaa-b3367d576463-826x0.png";
+        }
+    }
+}
---
+++ Lesson 33. WPF MVVM and DataGrid/WpMVVMDataGrid/WpMVVMDataGrid/Models/UserVM.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+
+namespace WpMVVMDataGrid.Models;
+
+/// <summary>
+/// INotifyPropertyChanged наслідуємо для реалізації MVVM патерну
+/// Має подію PropertyChanged, яка сповіщає View про зміни у ViewModel
+/// </summary>
+public class UserVM : INotifyPropertyChanged
+{
+    public int Id { get; set; }
+
+    private string _name;
+
+    private string _image;
+
+    public string Name
+    {
+        get { return _name; }
+        set 
+        {
+            if (_name != value) 
+            {
+                _name = value;
+                NotifyPropertyChanged("Name");
+            }
+             
+        }
+    }
+
+    public string Image
+    {
+        get { return _image; }
+        set
+        {
+            if (_image != value)
+            {
+                _image = value;
+                NotifyPropertyChanged("Image");
+            }
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// NotifyPropertyChanged(string propertyName) - 
+    /// метод, який викликає подію PropertyChanged
+    /// </summary>
+    public void NotifyPropertyChanged(string propertyName)
+    {
+        //Привязуємо властивість до події PropertyChanged
+        PropertyChanged?.Invoke(this, 
+            new PropertyChangedEventArgs(propertyName));
+    }
+}
